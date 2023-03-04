dc2b3095df46532edc7ccade587e99a7185965c7
hakim <hakim_yahi@hotmail.fr>
2023-03-04 12:49:14 +0100

    Ajout d'un premier ProblemDetail

---
+++ ProblemDetailFactory/Controllers/ErrorsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProblemDetailFactory.Controllers;
+
+[ApiController]
+public class ErrorsController : ControllerBase
+{
+    [Route("/error"),HttpGet]
+    public IActionResult Index()
+    {
+        var exceptionHandlerFeature = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error.Message;
+        return Problem(title:exceptionHandlerFeature);
+    }
+}
+
---
+++ ProblemDetailFactory/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Infrastructure;
+using ProblemDetailFactory.ErrorHandler;
+
+var builder = WebApplication.CreateBuilder(args);
+{
+    builder.Services.AddControllers();
+    builder.Services.AddSingleton<ProblemDetailsFactory, CustomProblemDetailsFactory>();
+}
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.UseExceptionHandler("/error");
+app.MapControllers();
+app.Run();
+
